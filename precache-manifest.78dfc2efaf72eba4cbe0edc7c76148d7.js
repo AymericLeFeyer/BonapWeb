d5c2083fe1784278046a24fe7f6aaf81d2803e75
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-05-09 22:28:58 +0200

    Updates

---
+++ precache-manifest.78dfc2efaf72eba4cbe0edc7c76148d7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09722480f5763ca081fc21058a532f0",
+    "url": "/BonapWeb/index.html"
+  },
+  {
+    "revision": "f9732d1062be0e6a3418",
+    "url": "/BonapWeb/static/css/main.859515bd.chunk.css"
+  },
+  {
+    "revision": "a089617e0f730ad9a135",
+    "url": "/BonapWeb/static/js/2.3a512772.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BonapWeb/static/js/2.3a512772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9732d1062be0e6a3418",
+    "url": "/BonapWeb/static/js/main.a50ee57c.chunk.js"
+  },
+  {
+    "revision": "820b927ddc503fb2b73a",
+    "url": "/BonapWeb/static/js/runtime-main.1a0be7b8.js"
+  },
+  {
+    "revision": "03d5249ae53462bc8e947213fca041f7",
+    "url": "/BonapWeb/static/media/bg.03d5249a.jpg"
+  },
+  {
+    "revision": "772cd56e7e013ce3c3fe7df02364df9d",
+    "url": "/BonapWeb/static/media/firebase.772cd56e.png"
+  },
+  {
+    "revision": "85afad5b9e3d82311724b55474aa0978",
+    "url": "/BonapWeb/static/media/flutter.85afad5b.png"
+  },
+  {
+    "revision": "5ffe3e02f6c399427ae17130f8cbe1b2",
+    "url": "/BonapWeb/static/media/instagram.5ffe3e02.png"
+  },
+  {
+    "revision": "2f23732c58402552c03b57e73c7ad826",
+    "url": "/BonapWeb/static/media/logo.2f23732c.png"
+  },
+  {
+    "revision": "2cf939357021890eaead95da888db5ff",
+    "url": "/BonapWeb/static/media/playStore.2cf93935.png"
+  }
+]);
